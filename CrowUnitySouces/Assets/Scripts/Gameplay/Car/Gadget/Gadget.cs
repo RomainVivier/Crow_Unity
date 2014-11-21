b0bfd7fed92ba92804f7b0403a42f32ee9f3db18
RomainVivier <romain.simon.vivier@gmail.com>
2014-11-21 15:34:45 +0100

    mise en place des premières classes pour les gadgets

---
+++ CrowUnitySouces/Assets/Scripts/Gameplay/Car/Gadget/Gadget.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gadget : MonoBehaviour {
+
+	private bool m_isReady = true;
+
+	public bool IsReady
+	{
+		get{ return m_isReady; }
+		set{ m_isReady = value; }
+	}
+
+	public virtual void Play()
+	{
+		IsReady = false;
+	}
+
+	public virtual void Stop()
+	{
+		IsReady = true;
+	}
+}
---
+++ CrowUnitySouces/Assets/Scripts/Gameplay/Car/Gadget/GadgetManager.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GadgetManager : MonoBehaviour {
+
+	#region Members
+
+	/// <summary>
+	/// key = event name
+	/// value = gadget
+	/// </summary>
+	private  Dictionary<string, Gadget> m_gadgets;
+	private static GadgetManager m_instance;
+
+	#endregion
+
+	#region Singleton
+
+	public static GadgetManager Instance
+	{
+		get
+		{
+			if (m_instance == null)
+			{
+				m_instance = GameObject.FindObjectOfType<GadgetManager>();
+			}
+			
+			return m_instance;
+		}
+	}
+	
+	void Awake()
+	{
+		if (m_instance == null)
+		{
+			m_instance = this;
+			m_instance.Init();
+		}
+		else
+		{
+			if (this != m_instance)
+				Destroy(this.gameObject);
+		}
+	}
+
+	private void Init()
+	{
+		m_gadgets = new Dictionary<string, Gadget>();
+	}
+
+	#endregion
+
+	#region Functions
+
+	public void Register(string name, Gadget gadget)
+	{
+		if(gadget == null || name == null || name == "" )
+		{
+			Debug.LogError("can't register null gadget or name can't be null or empty");
+			return;
+		}
+
+		m_gadgets.Add(name, gadget);
+	}
+
+	public void PlayGadget(string name)
+	{
+		if(m_gadgets.ContainsKey(name))
+		{
+			m_gadgets[name].Play();
+		}else{
+			Debug.Log("no gadget has been registered to this name");
+		}
+	}
+
+	#endregion
+
+}
