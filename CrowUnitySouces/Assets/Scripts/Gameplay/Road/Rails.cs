3d15bad03fb7e97167b7610b89077dcd24788488
Lilian Gimenez <lilian.gimenez@laposte.net>
2014-11-21 18:00:49 +0100

    Fixed serialisation in Rail

--- CrowUnitySouces/Assets/Scripts/Gameplay/Road/Rails.cs
+++ CrowUnitySouces/Assets/Scripts/Gameplay/Road/Rails.cs
@@ -1,13 +1,8 @@
-﻿using UnityEngine;
+﻿
+using UnityEngine;
 using UnityEditor;
 using System.Collections;
 
-[System.Serializable]
-class BezierPoint : ScriptableObject
-{
-	public Vector3 pos=Vector3.zero;
-	public Vector3 delta=Vector3.zero;
-}
 
 public class Rails : MonoBehaviour
 {
@@ -17,19 +12,19 @@ public class Rails : MonoBehaviour
 	public int nbPoints=0;
 	public bool drawShafts=false;
 	
-	[SerializeField]
-	BezierPoint[] points; // [point, rail]
-	[SerializeField]
-	Vector3[] positions;
-
-	[SerializeField, HideInInspector]	
-	private int nbPositions;
+	public Vector3[] positions;
+	public Vector3[] deltas;
+	
+	private int nbComputedPositions;
+	private Vector3[] computedPositions;
+	
+	
 	[SerializeField, HideInInspector]	
 	private int oldNbPoints=0;
 	[SerializeField, HideInInspector]	
 	private int oldNbRails=0;
 
-		void Start ()
+	void Start ()
 	{
 	}
 	
@@ -40,11 +35,15 @@ public class Rails : MonoBehaviour
 	
 	void OnValidate()
 	{
-		if(points==null)
+		if(positions==null)
 		{
-			points=new BezierPoint[0];
 			positions=new Vector3[0];
-			nbPositions=0;
+			deltas=new Vector3[0];
+			computedPositions=new Vector3[0];
+			nbPoints=0;
+			nbComputedPositions=0;
+			oldNbPoints=0;
+			oldNbRails=0;
 		}
 		if(oldNbPoints!=nbPoints || oldNbRails!=nbRails)
 		{
@@ -57,34 +56,46 @@ public class Rails : MonoBehaviour
 	
 	void resizeArray(int oldNbPoints, int oldNbRails)
 	{
-		BezierPoint[] newPoints=new BezierPoint[nbPoints*nbRails];
+		Vector3[] newPositions=new Vector3[nbPoints*nbRails];
+		Vector3[] newDeltas=new Vector3[nbPoints*nbRails];
 		for(int p=0;p<nbPoints;p++) for(int r=0;r<nbRails;r++)
 		{
-			if(p<oldNbPoints && r<oldNbRails) newPoints[p*nbRails+r]=points[p*nbRails+r];
-			else newPoints[p*nbRails+r]=new BezierPoint();
+			if(p<oldNbPoints && r<oldNbRails)
+			{
+				newPositions[p*nbRails+r]=positions[p*oldNbRails+r];
+				newDeltas[p*nbRails+r]=deltas[p*oldNbRails+r];				
+			}
+			else
+			{
+				newPositions[p*nbRails+r]=Vector3.zero;
+				newDeltas[p*nbRails+r]=Vector3.zero;			
+			}
 		}
-		nbPositions=(nbPoints-1)*NB_POSITIONS_PER_SEGMENT+1;
-		positions=new Vector3[nbPositions*nbRails];
-		points=newPoints;
+		nbComputedPositions=(nbPoints-1)*NB_POSITIONS_PER_SEGMENT+1;
+		computedPositions=new Vector3[nbComputedPositions*nbRails];
+		positions=newPositions;
+		deltas=newDeltas;
 	}
 	
 	void computePositions()
 	{
+		nbComputedPositions=(nbPoints-1)*NB_POSITIONS_PER_SEGMENT+1;
+		computedPositions=new Vector3[nbComputedPositions*nbRails];		
 		for(int r=0;r<nbRails;r++)
 		{
 			for(int p=0;p<nbPoints-1;p++)
 			{
-				BezierPoint pt=points[p*nbRails+r];
-				BezierPoint npt=points[(p+1)*nbRails+r];			
+				int i0=p*nbRails+r;
+				int i1=(p+1)*nbRails+r;
 				for(int sub=0;sub<NB_POSITIONS_PER_SEGMENT;sub++)
 				{
-					positions[(p*NB_POSITIONS_PER_SEGMENT+sub*nbRails)+r]=
-						bezier (new Vector3[4]{pt.pos,pt.pos+pt.delta,npt.pos-npt.delta,npt.pos},
+					computedPositions[(p*NB_POSITIONS_PER_SEGMENT+sub*nbRails)+r]=
+						bezier (new Vector3[4]{positions[i0],positions[i0]+deltas[i0],positions[i1]-deltas[i1],positions[i1]},
 									sub/(NB_POSITIONS_PER_SEGMENT+1f));
 					
 				}
 			}
-			positions[(nbPositions-1)*nbRails+r]=points[(nbPoints-1)*nbRails+r].pos;
+			computedPositions[(nbPoints-1)*nbRails+r]=positions[(nbPoints-1)*nbRails+r];
 		}
 	}
 	
@@ -105,25 +116,24 @@ public class Rails : MonoBehaviour
 	{
 		for(int r=0;r<nbRails;r++)
 		{
-		
 			for(int p=0;p<nbPoints;p++)
 			{
-				BezierPoint pt=points[p*nbRails+r];
+				int index=p*nbRails+r;
 				if(drawShafts)
 				{
 					Gizmos.color=new Color(0,1f,0.5f);
-					Vector3 shaftBottom=pt.pos;
+					Vector3 shaftBottom=positions[index];
 					shaftBottom.y=-100;
 					Vector3 shaftTop=shaftBottom;
 					shaftTop.y=100;
 					Gizmos.DrawLine(shaftBottom,shaftTop);	
 				}
 				Gizmos.color=new Color(0,0.5f,0.5f);
-				Gizmos.DrawLine(pt.pos-pt.delta,pt.pos+pt.delta);
+				Gizmos.DrawLine(positions[index]-deltas[index],positions[index]+deltas[index]);
 			}
 			Gizmos.color=new Color(0,0.5f,1);
-			for(int p=0;p<nbPositions-1;p++)
-				Gizmos.DrawLine(positions[p*nbRails+r],positions[(p+1)*nbRails+r]);
+			for(int p=0;p<nbPoints-1;p++)
+				Gizmos.DrawLine(computedPositions[p*nbRails+r],computedPositions[(p+1)*nbRails+r]);
 		}
 	}
 	
@@ -133,16 +143,17 @@ public class Rails : MonoBehaviour
 		void OnSceneGUI()
 		{
 			Rails tgt=(Rails) target;
+			if(tgt.nbPoints!=tgt.oldNbPoints || tgt.nbRails!=tgt.oldNbRails) return;
 	        for(int p=0;p<tgt.nbPoints;p++) for(int r=0;r<tgt.nbRails;r++)
 	        {
 				Handles.color=new Color(1,0.5f,0);
-				BezierPoint pt=tgt.points[p*tgt.nbRails+r];
-				pt.pos=Handles.FreeMoveHandle(pt.pos,Quaternion.identity,0.2f,Vector3.zero,Handles.SphereCap);
+				int index=p*tgt.nbRails+r;
+				tgt.positions[index]=Handles.FreeMoveHandle(tgt.positions[index],Quaternion.identity,0.2f,Vector3.zero,Handles.SphereCap);
 				Handles.color=new Color(0.5f,0.5f,0);
-				pt.delta=Handles.FreeMoveHandle(pt.pos+pt.delta,Quaternion.identity,0.2f,Vector3.zero,Handles.SphereCap)
-								-pt.pos;
-				pt.delta=pt.pos-
-     		             Handles.FreeMoveHandle(pt.pos-pt.delta,Quaternion.identity,0.2f,Vector3.zero,Handles.SphereCap);
+				tgt.deltas[index]=Handles.FreeMoveHandle(tgt.positions[index]+tgt.deltas[index],Quaternion.identity,0.2f,Vector3.zero,Handles.SphereCap)
+								-tgt.positions[index];
+				tgt.deltas[index]=tgt.positions[index]-
+     		             Handles.FreeMoveHandle(tgt.positions[index]-tgt.deltas[index],Quaternion.identity,0.2f,Vector3.zero,Handles.SphereCap);
 	        }
 	        tgt.computePositions();
 		}
