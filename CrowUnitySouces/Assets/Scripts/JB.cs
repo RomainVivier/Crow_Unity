283854cddf18e473196ffdb5ea8a1f3989c20cdb
Lilian Gimenez <lilian.gimenez@laposte.net>
2014-11-27 14:41:00 +0100

    Some work

---
+++ CrowUnitySouces/Assets/Scripts/JB.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JB : MonoBehaviour {
+
+    private bool active;
+
+	// Use this for initialization
+	void Start ()
+    {
+        active = false;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    public void activate()
+    {
+        active = true;
+    }
+}
---
+++ CrowUnitySouces/Assets/Scripts/JBTrigger.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JBTrigger : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        GameObject oth = other.transform.root.gameObject;
+        if(oth.name=="Car")
+        {
+            GameObject.Find("JB").GetComponent<JB>().activate();
+            oth.GetComponentInChildren<RailsControl>().setSpeedKmh *= 0.8f;
+        }
+    }
+}
