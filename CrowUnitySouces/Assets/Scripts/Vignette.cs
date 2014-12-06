dcebf7fb8d32eed3d07de12f295d9571443806c7
unknown <lilian.gimenez@STUD-VDOC-052.formation.enjmin.fr>
2014-12-06 15:28:56 +0100

    Basic vignettes

---
+++ CrowUnitySouces/Assets/Scripts/Vignette.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Vignette : MonoBehaviour
+{
+
+    public float defaultTime = 5;
+    private float remainingTime;
+
+	void Start ()
+    {
+	
+	}
+	
+    void Update()
+    {
+        remainingTime -= Time.deltaTime;
+        if (remainingTime <= 0) camera.enabled = false;
+    }
+
+    public void pop(float time=-1)
+    {
+        pop(camera.rect,time);
+    }
+
+    public void pop(Rect rect, float time=-1)
+    {
+        if (time == -1) time = defaultTime;
+        remainingTime = time;
+        camera.enabled = true;
+        camera.rect=rect;
+        float mainVfov = GameObject.Find("Car/Body/Camera").camera.fieldOfView;
+        camera.fieldOfView=Mathf.Atan(Mathf.Tan(Mathf.Deg2Rad*mainVfov)*rect.height)*Mathf.Rad2Deg;
+    }
+}
