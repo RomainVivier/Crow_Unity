33c90db5de64c224fc52fb3534b884da0e27b905
unknown <romain.vivier@STUD-VDOC-051.formation.enjmin.fr>
2014-12-06 17:16:09 +0100

    mise en place de texture sur les immeuble

--- CrowUnitySouces/Assets/Scripts/Gameplay/Road/BuildingGenerator.cs
+++ CrowUnitySouces/Assets/Scripts/Gameplay/Road/BuildingGenerator.cs
@@ -9,22 +9,34 @@ public class BuildingGenerator : MonoBehaviour {
 
 	void Start()
     {
-	    if(_minHeight < 1 || (_maxHeight < 1 || _maxHeight < _minHeight) || _theme == "" )
+	    if(_minHeight < 1 || (_maxHeight < 1 || _maxHeight < _minHeight) || _theme == "")
         {
             Debug.Log("Some parameters are wrong. Correct it and try again.");
             return;
         }
 
+        float randColor = Random.Range(0f, 1f);
+        ProceduralMaterial mat;
         int height = Random.Range(_minHeight, _maxHeight);
+        GameObject building;
 
-        GameObject building = GameObject.Instantiate(Resources.Load("Buildings/" + _theme + "_Base"), transform.position, Quaternion.Euler(new Vector3(-90, 90 + transform.rotation.eulerAngles.y, 0))) as GameObject;
-        building.transform.localScale = Vector3.Scale(building.transform.localScale, transform.localScale);
+        if (height != 1)
+        {
+            building = GameObject.Instantiate(Resources.Load("Buildings/" + _theme + "_Base"), transform.position, Quaternion.Euler(new Vector3(-90, 90 + transform.rotation.eulerAngles.y, 0))) as GameObject;
+            mat = building.renderer.materials[1] as ProceduralMaterial;
+            mat.SetProceduralFloat("Wall_Color", randColor);
+            building.renderer.materials[1] = mat;
+            building.transform.localScale = Vector3.Scale(building.transform.localScale, transform.localScale);
 
-        building.transform.parent = transform;
+            building.transform.parent = transform;
+        }
 
         for (int i = 1; i < (height - 1); i++)
         {
             building = GameObject.Instantiate(Resources.Load("Buildings/" + _theme + "_Middle"), transform.position + Vector3.up * 30 * i * transform.localScale.y, Quaternion.Euler(new Vector3(-90, 90 + transform.rotation.eulerAngles.y, 0))) as GameObject;
+            mat = building.renderer.materials[1] as ProceduralMaterial;
+            mat.SetProceduralFloat("Wall_Color", randColor);
+            building.renderer.materials[1] = mat;
             building.transform.localScale = Vector3.Scale(building.transform.localScale, transform.localScale);
             building.transform.parent = transform;
         }
@@ -32,6 +44,9 @@ public class BuildingGenerator : MonoBehaviour {
         if (height >= 1)
         {
             building = GameObject.Instantiate(Resources.Load("Buildings/" + _theme + "_Top"), transform.position + Vector3.up * 30 * (height - 1) * transform.localScale.y, Quaternion.Euler(new Vector3(-90, 90 + transform.rotation.eulerAngles.y, 0))) as GameObject;
+            mat = building.renderer.materials[1] as ProceduralMaterial;
+            mat.SetProceduralFloat("Wall_Color", randColor);
+            building.renderer.materials[1] = mat;
             building.transform.localScale = Vector3.Scale(building.transform.localScale, transform.localScale);
             building.transform.parent = transform;
         }
