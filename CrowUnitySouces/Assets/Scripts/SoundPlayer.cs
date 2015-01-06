6020a8c99e1bc89fccbb9c1fc1869c18eef71c9e
Lilian Gimenez <lilian.gimenez@laposte.net>
2015-01-06 17:19:18 +0100

    SoundPlayer for swoosh sounds

---
+++ CrowUnitySouces/Assets/Scripts/SoundPlayer.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundPlayer : MonoBehaviour
+{
+
+    #region Members
+
+    public string _soundName;
+    public bool _is3D=false;
+    public bool _onlyOnce = true;
+
+    private FMOD.Studio.EventInstance m_fmodEvent;
+    private bool m_alreadyPlayed;
+    #endregion
+
+    #region MonoBehaviour
+
+    void Start()
+    {
+        Debug.Log("coucou");
+        m_fmodEvent=FMOD_StudioSystem.instance.GetEvent("event:/"+_soundName);
+        m_alreadyPlayed = true;
+    }
+    #endregion
+
+    #region Collider
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Trigger");
+        if(other.gameObject.name=="Car" && (!_onlyOnce || m_alreadyPlayed))
+        {
+            m_alreadyPlayed=true;
+            m_fmodEvent.start();
+            if(!_is3D)
+            {
+                Vector3 dpos=transform.position-other.transform.position;
+                dpos.Normalize();
+                Vector3 right=other.transform.right;
+                float diff=Vector3.Dot(dpos,right);
+                FMOD.Studio.ParameterInstance param;
+                m_fmodEvent.getParameter("Pan",out param);
+                param.setValue(diff>0 ? 1 : 0); 
+            }
+        }
+    }
+    #endregion
+}
