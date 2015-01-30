3cc37aeb742224604b1d279e5dfff62675709d80
Lilian Gimenez <lilian.gimenez@laposte.net>
2015-01-30 10:06:33 +0100

    PrefabSpawner

---
+++ CrowUnitySouces/Assets/Scripts/Gameplay/Road/PrefabSpawner.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PrefabSpawner : MonoBehaviour
+{
+    #region members
+    public GameObject prefab;
+    private Vector3 m_pos;
+    private GameObject m_spawnedObject = null;
+    #endregion
+
+    #region methods
+    void Start ()
+    {
+        m_pos = new Vector3(float.NaN, float.NaN, float.NaN);
+	}
+	
+	void Update ()
+    {
+        Vector3 pos = transform.position;
+        if(pos!=m_pos)
+        {
+            m_pos = pos;
+            spawnPrefab();
+        }
+    }
+
+    private void spawnPrefab()
+    {
+        if (m_spawnedObject != null) GameObject.Destroy(m_spawnedObject);
+        m_spawnedObject=(GameObject) GameObject.Instantiate(prefab, transform.position, transform.rotation);
+    }
+    
+    void OnDrawGizmos()
+    {
+
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, 1);
+    }
+    #endregion
+}
