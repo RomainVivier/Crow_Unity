f7e290d68998ccb7ec38ac8e2bc1ee65fa0b5829
Lilian Gimenez <lilian.gimenez@laposte.net>
2015-02-03 18:01:22 +0100

    Graph stuff

---
+++ CrowUnitySouces/Assets/Scripts/AmbientLightChanger.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AmbientLightChanger : MonoBehaviour
+{
+    #region attributes
+
+    // Change this in-game
+    public Color AmbientLight
+    {
+        get { return RenderSettings.ambientLight; }
+        set { RenderSettings.ambientLight = value; }
+    }
+
+    // Change this in the editor
+    public Color c;
+
+    #endregion
+
+    #region methods
+    void Start ()
+    {
+	
+	}
+	
+	void Update ()
+    {
+
+    }
+    #endregion
+}
