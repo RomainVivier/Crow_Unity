968bfb3f2559697618669f09d43b5b56170e7118
Lilian Gimenez <lilian.gimenez@laposte.net>
2015-02-07 17:54:19 +0100

    Some unfinished stuff

--- CrowUnitySouces/Assets/Scripts/Gameplay/Car/CarCollisionsHandler.cs
+++ CrowUnitySouces/Assets/Scripts/Gameplay/Car/CarCollisionsHandler.cs
@@ -8,8 +8,8 @@ public class CarCollisionsHandler : MonoBehaviour
     public float _maxAngleHDeg = 45;
     public float _maxAngleVDeg = 45;
 
-    public float _minMomentum = 1000;// In Kg.m/s (or N.s)
-    public float _maxMomentum = 3000;
+    public float _minMomentum = 10000;// In Kg.m/s (or N.s)
+    public float _maxMomentum = 30000;
 
     #endregion
 
@@ -37,14 +37,15 @@ public class CarCollisionsHandler : MonoBehaviour
     #endregion
 
     #region Collider
-    void OnCollisionEnter(Collision collision)
+    
+    void OnTriggerEnter(Collider other)
     {
-        GameObject oth = collision.gameObject;
+        GameObject oth = other.gameObject;
         if (oth.name == "Obstacle_Car(Clone)")
         {
             if (cooldownTimer.IsElapsedLoop)
             {
-                playSound(collision, m_impactVehicleSound, m_impactVehicleSpeed);
+                playSound(null, oth, m_impactVehicleSound, m_impactVehicleSpeed);
                 cooldownTimer.Reset(2f);
             }
             float hAngle = Random.Range(-_maxAngleHDeg, _maxAngleHDeg) * Mathf.Deg2Rad;
@@ -57,31 +58,44 @@ public class CarCollisionsHandler : MonoBehaviour
             float momentum=Mathf.Lerp(_minMomentum,_maxMomentum,m_car.getForwardVelocityKmh()/m_car.maxSpeedKmh);
             oth.rigidbody.AddForce(direc * momentum);
         }
-        else if (!collision.collider.isTrigger)
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        GameObject oth = collision.gameObject;
+        if (!collision.collider.isTrigger)
         {
             Vector3 contactPoint = collision.contacts[0].point;
             Vector3 center = transform.position;
             Vector3 diff = contactPoint - center;
             float relDy = Vector3.Dot(transform.up, diff);
-            if (relDy > -0.7) playSound(collision, m_impactConcreteSound, m_impactConcreteSpeed);
+            if (relDy > -0.7) playSound(collision, oth, m_impactConcreteSound, m_impactConcreteSpeed);
         }
     }
     #endregion
 
     #region private methods
-    void playSound(Collision collision, FMOD.Studio.EventInstance sound, FMOD.Studio.ParameterInstance param)
+    void playSound(Collision collision, GameObject oth, FMOD.Studio.EventInstance sound, FMOD.Studio.ParameterInstance param)
     {
         FMOD.Studio._3D_ATTRIBUTES threeDeeAttr = new FMOD.Studio._3D_ATTRIBUTES();
-        threeDeeAttr.position = FMOD.Studio.UnityUtil.toFMODVector(collision.contacts[0].point);
         threeDeeAttr.up = FMOD.Studio.UnityUtil.toFMODVector(new Vector3(0,1,0));
-        threeDeeAttr.forward = FMOD.Studio.UnityUtil.toFMODVector(collision.contacts[0].normal);
+        if(collision!=null)
+        {
+            threeDeeAttr.forward = FMOD.Studio.UnityUtil.toFMODVector(collision.contacts[0].normal);
+            threeDeeAttr.position = FMOD.Studio.UnityUtil.toFMODVector(collision.contacts[0].point);
+        }
+        else
+        {
+            threeDeeAttr.forward = FMOD.Studio.UnityUtil.toFMODVector(m_car.getForwardVector());
+            threeDeeAttr.position = FMOD.Studio.UnityUtil.toFMODVector(oth.transform.position);
+        }
         threeDeeAttr.velocity = FMOD.Studio.UnityUtil.toFMODVector(Vector3.zero);
         sound.set3DAttributes(threeDeeAttr);
         float maxSpeed = m_car.maxSpeedKmh;
         RailsControl rc = m_car.gameObject.GetComponent<RailsControl>();
         if (rc != null) maxSpeed = rc.setSpeedKmh;
         Vector3 relativeVelocity = m_car.getVelocity();
-        Rigidbody body=collision.gameObject.rigidbody;
+        Rigidbody body=collision==null ? oth.rigidbody : collision.gameObject.rigidbody;
         //if(body!=null) relativeVelocity-=body.GetRelativePointVelocity(new Vector3(0, 0, 0));
         param.setValue(Mathf.Min(relativeVelocity.magnitude*3.6f / maxSpeed,1));
         sound.start();
