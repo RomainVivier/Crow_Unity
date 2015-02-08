d056ce472fef49da4510a2eb9b459a0417d01a09
Lilian Gimenez <lilian.gimenez@laposte.net>
2015-02-08 15:26:44 +0100

    Working fake collisions

--- CrowUnitySouces/Assets/Scripts/Gameplay/Car/CarCollisionsHandler.cs
+++ CrowUnitySouces/Assets/Scripts/Gameplay/Car/CarCollisionsHandler.cs
@@ -56,7 +56,7 @@ public class CarCollisionsHandler : MonoBehaviour
             Vector3 up = m_car.getUpVector();
             Vector3 direc = hVector * Mathf.Cos(vAngle) + up * Mathf.Sin(vAngle);
             float momentum=Mathf.Lerp(_minMomentum,_maxMomentum,m_car.getForwardVelocityKmh()/m_car.maxSpeedKmh);
-            oth.rigidbody.AddForce(direc * momentum);
+            oth.rigidbody.AddForce(direc * momentum,ForceMode.Impulse);
         }
     }
 
