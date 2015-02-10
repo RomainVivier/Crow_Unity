4351068b0e279692274654c6f0ce5dd3bc6e83db
Lilian Gimenez <lilian.gimenez@laposte.net>
2015-02-10 15:10:13 +0100

    Cards

---
+++ CrowUnitySouces/Assets/Scripts/Gameplay/Car/Gadget/CardPopup.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CardPopup : MonoBehaviour
+{
+    #region attributes
+    private const float TIMER_DURATION=1f;
+    private Timer m_cardTimer;
+    private Material m_currentMaterial;
+    private Material m_targetMaterial;
+    private Animator m_animator;
+    private MeshRenderer m_meshRenderer;
+    #endregion
+
+    #region methods
+    void Start ()
+    {
+        GadgetManager.Instance.CardPopup = this;
+        m_animator = gameObject.GetComponent<Animator>();
+        m_meshRenderer = gameObject.GetComponent<MeshRenderer>();
+        m_cardTimer=new Timer();
+	}
+	
+	void Update ()
+    {
+        if(m_currentMaterial!=m_targetMaterial)
+        {
+            if (m_animator.GetCurrentAnimatorStateInfo(0).nameHash == -1783470761)//Animator.StringToHash("Down"))
+                popupNow();
+        }
+        if (m_cardTimer.IsElapsedOnce) m_animator.SetBool("isUp", false);
+    }
+
+    public void popup(Material newMaterial)
+    {
+        m_targetMaterial = newMaterial;
+        if (m_animator.GetCurrentAnimatorStateInfo(0).nameHash == -1783470761)//Animator.StringToHash("Down"))
+            popupNow();
+        else m_animator.SetBool("isUp", false);
+        Debug.Log(m_animator.GetCurrentAnimatorStateInfo(0).nameHash);
+    }
+
+    private void popupNow()
+    {
+        m_currentMaterial = m_targetMaterial;
+        m_animator.SetBool("isUp", true);
+        m_cardTimer.Reset(TIMER_DURATION);
+        m_meshRenderer.material = m_currentMaterial;
+    }
+    #endregion
+}
