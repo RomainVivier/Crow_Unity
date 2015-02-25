67a7c93f4403a72fcf004af2d72c6fd3b149a364
Kratorg <romain.vivier@STUD-VDOC-051.formation.enjmin.fr>
2015-02-25 16:54:02 +0100

    vignette

--- CrowUnitySouces/Assets/Scripts/Gameplay/Road/Obstacle.cs
+++ CrowUnitySouces/Assets/Scripts/Gameplay/Road/Obstacle.cs
@@ -6,11 +6,12 @@ public abstract class Obstacle : MonoBehaviour
 {
     #region members
 
+    public GameObject _vignette;
+
     protected List<GadgetAbility> m_weaknesses;
     protected Rails m_rails;
     protected float m_railsIndex;
     protected float m_railsProgress;
-
     #endregion
 
     public Rails Rails
@@ -34,6 +35,21 @@ public abstract class Obstacle : MonoBehaviour
     public virtual void Start()
     {
         m_weaknesses = new List<GadgetAbility>();
+        _vignette.SetActive(false);
+    }
+
+    public virtual void Update()
+    {
+        if(Vector3.Distance(Score.Instance.Body.transform.position, transform.position) < 150f && !_vignette.activeSelf)
+        {
+            _vignette.SetActive(true);
+        }
+
+        if (Vector3.Distance(Score.Instance.Body.transform.position, transform.position) < 50f && _vignette.activeSelf)
+        {
+            _vignette.SetActive(false);
+        }
+
     }
 
     public virtual void Behaviour()
