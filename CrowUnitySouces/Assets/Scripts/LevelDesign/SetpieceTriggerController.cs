d47e5002a887a01ef4cff5476e25b9f4d0ed1b15
unknown <christophe.lionet@PC-VD-CNAM48.formation.enjmin.fr>
2015-02-28 14:34:00 +0100

    Création de branche et système de génération de route irrégulière pour tests.

---
+++ CrowUnitySouces/Assets/Scripts/LevelDesign/SetpieceTriggerController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetpieceTriggerController : MonoBehaviour {
+
+	BigBaddieController m_baddieController;
+
+	// Use this for initialization
+	void Start () {
+		m_baddieController = GameObject.Find ("Baddie").GetComponent<BigBaddieController>();
+	}
+
+	public void SetBaddieInterpolationTime(float time)
+	{
+		m_baddieController.SetInterpolationTime (time);
+	}
+	public void SetBaddieTarget(Transform target)
+	{
+		m_baddieController.MoveTo (target);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
