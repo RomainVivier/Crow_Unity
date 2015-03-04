37aab1f60d885a214294eed137761f7edf299f36
Kratorg <romain.vivier@STUD-VDOC-051.formation.enjmin.fr>
2015-03-04 11:11:15 +0100

    update turret

--- CrowUnitySouces/Assets/Scripts/Gameplay/Road/Obstacles/Turret.cs
+++ CrowUnitySouces/Assets/Scripts/Gameplay/Road/Obstacles/Turret.cs
@@ -7,6 +7,8 @@ public class Turret : Obstacle
     public float _fireRate;
     public Transform _canon;
     public GameObject _projectile;
+    public Color _color;
+    public Color _shootColor;
 
     private Timer m_timer;
 
@@ -25,9 +27,15 @@ public class Turret : Obstacle
             return;
         }
 
+        if(m_timer.Current < _fireRate/3)
+        {
+            renderer.material.color = _shootColor;
+        }
+
         if(m_timer.IsElapsedOnce)
         {
             Fire();
+            renderer.material.color = _color;
         }
     }
 
