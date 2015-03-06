04ca4bb28c46d71fc7a027143906c583cb85a34b
Kratorg <romain.vivier@STUD-VDOC-051.formation.enjmin.fr>
2015-03-06 11:51:32 +0100

    the burger is now running for you

--- CrowUnitySouces/Assets/Scripts/Gameplay/Road/Obstacles/FallingBurger.cs
+++ CrowUnitySouces/Assets/Scripts/Gameplay/Road/Obstacles/FallingBurger.cs
@@ -7,23 +7,9 @@ public class FallingBurger : Obstacle
     public GameObject _prefabBurger;
     public Animator _anim;
 
-    //public override void Update()
-    //{
-    //    if (Score.Instance.Body.transform.position.x > transform.position.x)
-    //    {
-    //        m_activated = false;
-    //    }
-
-    //    base.Update();
-    //}
-
     public override void Activate()
     {
         base.Activate();
-        //if (_anim.GetCurrentAnimationClipState(0)[0].clip.name != "Idle")
-        //{
-        //    _anim.SetTrigger("Reset"); 
-        //}
         GameObject burger = GameObject.Instantiate(_prefabBurger, _burgerRoot.position, Quaternion.Euler(Vector3.right * (-90))) as GameObject;
         burger.transform.parent = _burgerRoot;
         burger.transform.localScale = new Vector3(0.566f, 0.7533f, 0.028f);
