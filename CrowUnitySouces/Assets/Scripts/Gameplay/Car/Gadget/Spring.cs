2da53596e5c69d18f3db9dea01aeed8ec3d901aa
Lilian Gimenez <lilian.gimenez@laposte.net>
2015-03-06 14:39:52 +0100

    Spring fix

--- CrowUnitySouces/Assets/Scripts/Gameplay/Car/Gadget/Spring.cs
+++ CrowUnitySouces/Assets/Scripts/Gameplay/Car/Gadget/Spring.cs
@@ -80,7 +80,8 @@ public class Spring : Gadget
                 else
                 {
                     Vector3 forwardTarget = m_car.getDeltaTarget();
-                    forwardTarget.y = 0;
+                    //forwardTarget.y = 0;
+                    forwardTarget.y += m_addPos.y;
                     forwardTarget.Normalize();
                     m_addPos += forwardTarget * Time.fixedDeltaTime * _glidingDist / _glidingTime;
                     m_carBodyTransform.position = m_basePos + m_addPos;
