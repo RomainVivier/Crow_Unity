2af1bb0e97d7c6d43449032f53621df5ad3342fd
unknown <lilian.gimenez@STUD-VDOC-052.formation.enjmin.fr>
2015-03-09 17:10:48 +0100

    Rocket fx

--- CrowUnitySouces/Assets/Scripts/RainbowPointLight.cs
+++ CrowUnitySouces/Assets/Scripts/RainbowPointLight.cs
@@ -3,35 +3,41 @@ using System.Collections;
 
 public class RainbowPointLight : MonoBehaviour {
 
-    public float _gradiantSpeed=1;
     public ParticleSystem _particlesSystem;
+	public Color[] _colors;
+	public float _time;
+	public bool loop = false;
 
     private float m_gradiantPos;
     private Light m_light;
-
+	private int m_nbColors;
+	
 	void Start ()
     {
         m_light = GetComponent<Light>();
+        m_nbColors=_colors.Length;
 	}
 	
 	void Update ()
     {
-	    if(_particlesSystem.isPlaying)
+	    if(_particlesSystem==null || _particlesSystem.isPlaying)
         {
             m_light.enabled = true;
-            m_gradiantPos += _gradiantSpeed * Time.deltaTime;
-            Color color=Color.black;
-            float inPos = (m_gradiantPos * 6) % 1;
-            int outPos = (Mathf.FloorToInt(m_gradiantPos * 6))%6;
-            switch(outPos)
-            {
-                case 0:color = new Color(1, 0, inPos);break;
-                case 1:color = new Color(1-inPos, 0, 1);break;
-                case 2:color = new Color(0, inPos, 1); break;
-                case 3:color = new Color(0, 1, 1-inPos); break;
-                case 4:color = new Color(inPos, 1, 0); break;
-                case 5:color = new Color(1, 1-inPos, 0); break;
-            }
+            m_gradiantPos += Time.deltaTime/_time;
+            //Color color=Color.black;
+			if(!loop && m_gradiantPos>(m_nbColors-2f)/(m_nbColors-1f)) m_gradiantPos=(m_nbColors-2f)/(m_nbColors-1f);
+				
+            float inPos = (m_gradiantPos * m_nbColors) % 1;
+            int outPos = (Mathf.FloorToInt(m_gradiantPos * m_nbColors))%m_nbColors;
+            Color color, prevColor,nextColor;
+            prevColor=_colors[outPos];
+            nextColor=_colors[(outPos+1)%m_nbColors];
+            
+            color.r=Mathf.Lerp(prevColor.r,nextColor.r,inPos);
+			color.g=Mathf.Lerp(prevColor.g,nextColor.g,inPos);
+			color.b=Mathf.Lerp(prevColor.b,nextColor.b,inPos);
+			color.a=Mathf.Lerp(prevColor.a,nextColor.a,inPos);
+
             m_light.color = color;
         }
         else
@@ -39,5 +45,10 @@ public class RainbowPointLight : MonoBehaviour {
             m_gradiantPos = 0;
             m_light.enabled = false;
         }
+	}
+	
+	public void Reset()
+	{
+		m_gradiantPos=0;
 	}
 }
