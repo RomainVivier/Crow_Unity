f98ae4350162abbcf29d475c37a19945500ebd00
Kratorg <romain.vivier@STUD-VDOC-051.formation.enjmin.fr>
2015-03-10 14:43:48 +0100

    pauffinage du burger

--- CrowUnitySouces/Assets/Scripts/Gameplay/Road/Obstacles/Burger.cs
+++ CrowUnitySouces/Assets/Scripts/Gameplay/Road/Obstacles/Burger.cs
@@ -5,10 +5,16 @@ public class Burger : MonoBehaviour
 {
     public Vector3 _direction;
     public float _speed;
+	public ParticleSystem _ps; 
 
     void Update()
     {
         transform.position += _direction * Time.deltaTime * _speed;
     }
 
+	public void Bounce()
+	{
+		_ps.Stop();
+		_ps.Play ();
+	}
 }
