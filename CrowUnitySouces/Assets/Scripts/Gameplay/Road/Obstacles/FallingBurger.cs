22580c08970bbafde2eebe76fad87cc8fd4c2fe7
Kratorg <romain.vivier@STUD-VDOC-051.formation.enjmin.fr>
2015-03-12 14:55:55 +0100

    turret and lateral burger

--- CrowUnitySouces/Assets/Scripts/Gameplay/Road/Obstacles/FallingBurger.cs
+++ CrowUnitySouces/Assets/Scripts/Gameplay/Road/Obstacles/FallingBurger.cs
@@ -6,14 +6,18 @@ public class FallingBurger : Obstacle
     public Transform _burgerRoot;
     public GameObject _prefabBurger;
     public Animator _anim;
+	public bool _instantiateBurger = true;
 
     public override void Activate()
     {
         base.Activate();
-        GameObject burger = GameObject.Instantiate(_prefabBurger, _burgerRoot.position, Quaternion.Euler(Vector3.right * (-90))) as GameObject;
-        burger.transform.parent = _burgerRoot;
-        burger.transform.localScale = new Vector3(0.566f, 0.7533f, 0.028f);
-        _anim.SetTrigger("Engage");
+		if(_instantiateBurger)
+		{
+	        GameObject burger = GameObject.Instantiate(_prefabBurger, _burgerRoot.position, Quaternion.Euler(Vector3.right * (-90))) as GameObject;
+	        burger.transform.parent = _burgerRoot;
+	        burger.transform.localScale = new Vector3(0.566f, 0.7533f, 0.028f);
+		}
+		_anim.SetTrigger("Engage");
         m_state = State.Activated;
     }
 
